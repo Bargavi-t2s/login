598b003335dae9ebc4bd0a9b67ad899e0aa3d130
Bargavi-t2s <59874562+Bargavi-t2s@users.noreply.github.com>
2020-02-28 11:44:12 +0530

    Update passport-setup.js
    path changes

--- src/controllers/passport-setup.js
+++ src/controllers/passport-setup.js
@@ -2,7 +2,7 @@ const passport=require('passport');
 const googlestrategy =require('passport-google-oauth20');
 passport.use(
     new googlestrategy({
-        callbackURL:'http://localhost:3082/home',
+        callbackURL:'http://ms41.t2scdn.com/google/redirect',
         clientID:"665557267772-0qd6ju1p6j0c0f56npft5eavp3ii7edv.apps.googleusercontent.com",
         clientSecret:"Nh9W8j_2-sIu4kcRTEeOOq9V"
 },(accessToken,refreshToken,profile,done)=>{
