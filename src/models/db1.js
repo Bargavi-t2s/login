b955f80e038aaa096329b58478214b5be1c877c9
Bargavi-t2s <bargavi.m@touch2success.com>
2020-03-03 17:18:15 +0530

    simple

--- src/models/db1.js
+++ src/models/db1.js
@@ -16,7 +16,7 @@ const dbName = 'loginapp';
 // Use connect method to connect to the server
 MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, client) {
   assert.equal(null, err);
-  console.log("Connected successfully to database via mongo");
+  console.log("Connected successfully to database via mongono");
 
   db = client.db(dbName);
   //console.log(db);
