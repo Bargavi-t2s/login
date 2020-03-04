62f015385b7750f3a2dce915b1b32401aec4c05a
Bargavi-t2s <bargavi.m@touch2success.com>
2020-03-04 18:03:10 +0530

    with changes of ufw

--- src/models/db1.js
+++ src/models/db1.js
@@ -8,14 +8,14 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = 'mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+const url = 'mongodb://127.0.0.1:27017';
 
 
 // Database Name
 const dbName = 'loginapp';
 
-// Use connect method to connect to the server
-MongoClient.connect(url,{ useUnifiedTopology: false }, function(err, client) {
+// Use connect method to connect to the servergit 
+MongoClient.connect(url,{ useUnifiedTopology:true }, function(err, client) {
   assert.equal(null, err);
 
   console.log("Connected successfully to database via mongono");
