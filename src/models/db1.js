e2003d088927533f030299e615c06b70dde12333
Bargavi-t2s <bargavi.m@touch2success.com>
2020-03-04 12:17:20 +0530

    unifiedtopolgy has been changed

--- src/models/db1.js
+++ src/models/db1.js
@@ -15,7 +15,7 @@ const url = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%2
 const dbName = 'loginapp';
 
 // Use connect method to connect to the server
-MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, client) {
+MongoClient.connect(url,{ useUnifiedTopology: false }, function(err, client) {
   assert.equal(null, err);
 
   console.log("Connected successfully to database via mongono");
