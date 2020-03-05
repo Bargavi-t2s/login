5437aa1a79c37121c2837ff3d2caf9a8d3edfd1d
Bargavi-t2s <bargavi.m@touch2success.com>
2020-03-05 12:31:51 +0530

    new url

--- src/models/db1.js
+++ src/models/db1.js
@@ -10,7 +10,7 @@ const assert = require('assert');
 // Connection URL
 const url = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
-
+console.log(url);
 // Database Name
 const dbName = 'loginapp';
 
